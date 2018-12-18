558ede1e571e601c51725e94226f7b1ebfbd93e5
BryanPinto <bryaan.pintoo@gmail.com>
2018-12-18 14:15:39 -0300

    Parametro nulo de metodos

---
+++ wsEmaresaWCF/EmaresaService.svc.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace wsEmaresaWCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class EmaresaService : IEmaresaService
+    {
+        public string GetData()
+        {
+            string value = "prueba";
+            return value;
+        }
+
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        //public ServiciosWeb GetDataUsingDataContract(ServiciosWeb composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
+
+        public string GetJSONtoXML(string json)
+        {
+            try
+            {
+                var xmlNode = JsonConvert.DeserializeXmlNode(json).OuterXml;
+                //Escribir log
+                string rutaLog = HttpRuntime.AppDomainAppPath;
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append(Environment.NewLine +
+                          DateTime.Now.ToShortDateString() + " " +
+                          DateTime.Now.ToShortTimeString() + ": " +
+                          "[ConvertirJSONaXML] -- JSON: " + json + " | " + "XML: " + xmlNode);
+                System.IO.File.AppendAllText(rutaLog + "Log.txt", sb.ToString());
+                sb.Clear();
+                return xmlNode;
+            }
+            catch (Exception ex)
+            {
+                //Replicar formato JSON para la respuesta error del método
+                string salida = "{\"Error\":\"" + ex.Message + "\"}";
+                //Escribir log
+                string rutaLog = HttpRuntime.AppDomainAppPath;
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append(Environment.NewLine +
+                          DateTime.Now.ToShortDateString() + " " +
+                          DateTime.Now.ToShortTimeString() + ": " +
+                          "[ConvertirJSONaXML] -- Salida: " + salida);
+                System.IO.File.AppendAllText(rutaLog + "Log.txt", sb.ToString());
+                sb.Clear();
+                //retornar salida
+                return salida;
+            }
+        }
+
+        public string GetOK(string jsonString)
+        {
+            try
+            {
+                string respuesta = "OK";
+                //variable para lanzar número al azar entre 0 y 10
+                var NumAzar = new Random().Next(0, 10);
+
+                if (NumAzar == 1 || NumAzar == 8)
+                {
+                    throw new Exception("Error en respuesta");
+                }
+                else
+                {
+                    //Replicar formato JSON para la respuesta del método
+                    string salida = "{\"Respuesta\":\"" + respuesta + "\"}";
+                    //Escribir log
+                    string rutaLog = HttpRuntime.AppDomainAppPath;
+                    StringBuilder sb = new StringBuilder();
+
+                    sb.Append(Environment.NewLine +
+                              DateTime.Now.ToShortDateString() + " " +
+                              DateTime.Now.ToShortTimeString() + ": " +
+                              "[RetornaOK] -- Entrada: " + jsonString + " | " + "Salida: " + salida);
+                    System.IO.File.AppendAllText(rutaLog + "Log.txt", sb.ToString());
+                    sb.Clear();
+
+                    //declarar xml de creación
+                    string xmlCreacion = @"<?xml version=""1.0""?>
+                                            <BizAgiWSParam>
+                                                <domain>domain</domain>
+                                                <userName>admon</userName>
+                                                <Cases>
+                                                    <Case>
+                                                        <Process>CopyProcesoDeCompras</Process>
+                                                        <Entities>
+                                                            <ProcesodeCompras>
+                                                                <NroSolicitudERP>ASD</NroSolicitudERP>
+                                                                <CentrodeCostos>ASD</CentrodeCostos>
+                                                                <FechaCotizacion>2018-12-11</FechaCotizacion>
+                                                                <FechaSolicitud>2018-12-11</FechaSolicitud>
+                                                                <Solicitante>ASD</Solicitante>
+                                                                <Condp_Pago>ASD</Condp_Pago>
+                                                                <Tipo_compra>ASD</Tipo_compra>
+                                                                <ObservacionSolicitud>ASD</ObservacionSolicitud>
+                                                                <Itemgeneral>1</Itemgeneral>
+                                                                <ItemSolicitud>1</ItemSolicitud>
+                                                                <TotalCotizado>1</TotalCotizado>
+                                                                <DetalleCotizacion>
+                                                                    <CodProducto>ASD</CodProducto>
+                                                                    <Tipo_Concepto>ASD</Tipo_Concepto>
+                                                                    <DescripcionAmpliada>ASD</DescripcionAmpliada>
+                                                                    <NombreProveedor>ASD</NombreProveedor>
+                                                                    <Cantidad>ASD</Cantidad>
+                                                                    <UnidadMedida>ASD</UnidadMedida>
+                                                                    <PrecioUnit>ASD</PrecioUnit>
+                                                                    <Neto>ASD</Neto>
+                                                                    <Observacion>ASD</Observacion>
+                                                                    <MotivoRechazo>ASD</MotivoRechazo>
+                                                                </DetalleCotizacion>
+                                                            </ProcesodeCompras>
+                                                        </Entities>
+                                                    </Case>
+                                                </Cases>
+                                            </BizAgiWSParam>";
+                    //crear instancia
+                    //BizagiCapaSOA.WorkflowEngineSOASoapClient serviceEngine = new BizagiCapaSOA.WorkflowEngineSOASoapClient();
+                    //AGREGAR REFERENCIA A CAPA SOA BIZAGI
+
+                    //string respuestaBizagi = serviceEngine.createCasesAsString(xmlCreacion);
+                    //retornar salida
+                    return salida;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Replicar formato JSON para la respuesta error del método
+                string salida = "{\"Error\":\"" + ex.Message + "\"}";
+                //Escribir log
+                string rutaLog = HttpRuntime.AppDomainAppPath;
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append(Environment.NewLine +
+                          DateTime.Now.ToShortDateString() + " " +
+                          DateTime.Now.ToShortTimeString() + ": " +
+                          "[RetornaOK] -- Entrada: " + jsonString + " | " + "Salida: " + salida);
+                System.IO.File.AppendAllText(rutaLog + "Log.txt", sb.ToString());
+                sb.Clear();
+                //retornar salida
+                return salida;
+            }
+        }
+
+        public string GetXMLtoJSON(string xml)
+        {
+            try
+            {
+                //Declarar variable vacía para convertir el xml a json 
+                string json = string.Empty;
+                //Generar un nuevo documento XML
+                XmlDocument doc = new XmlDocument();
+                //Asignar al documento el XML enviado
+                doc.LoadXml(xml);
+                //Utilizar variable json para realizar conversión
+                json = JsonConvert.SerializeXmlNode(doc);
+                //Escribir log
+                string rutaLog = HttpRuntime.AppDomainAppPath;
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append(Environment.NewLine +
+                          DateTime.Now.ToShortDateString() + " " +
+                          DateTime.Now.ToShortTimeString() + ": " +
+                          "[ConvertirXMLaJSON] -- XML: " + xml + " | " + "Salida JSON: " + json);
+                System.IO.File.AppendAllText(rutaLog + "Log.txt", sb.ToString());
+                sb.Clear();
+                //retornar salida
+
+                return (json);
+            }
+            catch (Exception ex)
+            {
+                //Replicar formato JSON para la respuesta error del método
+                string salida = "{\"Error\":\"" + ex.Message + "\"}";
+                //Escribir log
+                string rutaLog = HttpRuntime.AppDomainAppPath;
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append(Environment.NewLine +
+                          DateTime.Now.ToShortDateString() + " " +
+                          DateTime.Now.ToShortTimeString() + ": " +
+                          "[ConvertirXMLaJSON] -- Salida: " + salida);
+                System.IO.File.AppendAllText(rutaLog + "Log.txt", sb.ToString());
+                sb.Clear();
+                //retornar salida
+                return salida;
+            }
+        }
+    }
+}
---
+++ wsEmaresaWCF/IEmaresaService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace wsEmaresaWCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IEmaresaService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/GetData")]
+        string GetData();
+
+        //[OperationContract]
+        //ServiciosWeb GetDataUsingDataContract(ServiciosWeb composite);
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/GetOK", 
+            Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare)]
+        
+        string GetOK(string jsonString);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/GetJSONtoXML", 
+            Method = "POST",
+            //ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare)]  
+        string GetJSONtoXML(string json);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/GetXMLtoJSON", 
+            Method = "POST",
+            ResponseFormat = WebMessageFormat.Json)]
+        
+        string GetXMLtoJSON(string xml);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class ServiciosWeb
+    {
+        [DataMember]
+        public int value;
+        [DataMember]
+        public string jsonString { set; get; }
+        [DataMember]
+        public string xml { set; get; }
+        [DataMember]
+        public string json { set; get; }
+        [DataMember]
+        public string test { set; get; }
+    }
+}
